7a70e99eaf8f3cae7aa62168a91e6dee1216e60a
Mr-huangyh <huangyh@huangyh.top>
2020-01-28 22:14:26 +0800

    updata

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+
+var getAreaStat = () => {
+    return axios.get('https://3g.dxy.cn/newh5/view/pneumonia').then((data) => {
+        return data.data.split('.getAreaStat =')[1].split('}catch(e){}</script><script id="getIndex')[0];
+    })
+}
+
+app.get('/', async (req, res) => {
+    res.json(eval (await getAreaStat()));
+})
+
+app.listen(8888, () => {
+    console.log('run');
+})
